e4ae8dbe6587d14084debd36e222c6d78a49b1d8
renzroque <renzmroq@gmail.com>
2017-11-28 07:46:39 +1100

    created django model and views

--- vibrato_project/vibrato_app/admin.py
+++ vibrato_project/vibrato_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from vibrato_app.models import User
 
 # Register your models here.
+admin.site.register(User)
+
--- vibrato_project/vibrato_app/views.py
+++ vibrato_project/vibrato_app/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from vibrato_app.models import User
 
 # Create your views here.
+
+def index(request):
+	users_list = User.objects.all()
+	users_dict = { "users": users_list }
+
+	return render(request, 'index.html', context=users_dict)
+
+
