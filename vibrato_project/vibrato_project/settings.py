333ee709853af1473c657839777afb56273b9523
renzroque <renzmroq@gmail.com>
2017-11-28 08:28:47 +1100

    changed DB connection settings, now using mysql && fix template typo error

--- vibrato_project/vibrato_project/settings.py
+++ vibrato_project/vibrato_project/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'vibrato_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'vibratoDB',
+        'USER': 'admin',
+        'PASSWORD': 'v1br@t0',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
